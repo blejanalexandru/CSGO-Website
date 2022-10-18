78a60409b51ebab45b25119e6325006dd6e79d83
AlexISprogramming <34192745+blejanalexandru@users.noreply.github.com>
2022-10-18 18:05:16 +0300

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,76 @@
+let img = document.querySelector('#deliver');
+let btn1 = document.querySelector('#btn1');
+let btn2 = document.querySelector('#btn2');
+let btn3 = document.querySelector('#btn3');
+let btn4 = document.querySelector('#btn4');
+let btn5 = document.querySelector('#btn5');
+let btn6 = document.querySelector('#btn6');
+let btn7 = document.querySelector('#btn7');
+let btn8 = document.querySelector('#btn8');
+let btn9 = document.querySelector('#btn9');
+let btn10 = document.querySelector('#btn10');
+let btn11 = document.querySelector('#btn11');
+let btn12 = document.querySelector('#btn12');
+let btn13 = document.querySelector('#btn13');
+let btn14 = document.querySelector('#btn14');
+let btn15 = document.querySelector('#btn15')
+
+btn1.addEventListener('click', () => {
+    deliver.src = 'brackets/bracket 15.png';
+})
+
+btn2.addEventListener('click', () => {
+    deliver.src = 'brackets/bracket 14.png';
+})
+
+btn3.addEventListener('click', () => {
+    deliver.src = 'brackets/bracket 13.png';
+})
+
+btn4.addEventListener('click', () => {
+    deliver.src = 'brackets/bracket 12.png';
+})
+
+btn5.addEventListener('click', () => {
+    deliver.src = 'brackets/bracket 11.png';
+})
+
+btn6.addEventListener('click', () => {
+    deliver.src = 'brackets/bracket 10.png';
+})
+
+btn7.addEventListener('click', () => {
+    deliver.src = 'brackets/bracket 9.png';
+})
+
+btn8.addEventListener('click', () => {
+    deliver.src = 'brackets/bracket 8.png';
+})
+
+btn9.addEventListener('click', () => {
+    deliver.src = 'brackets/bracket 7.png';
+})
+
+btn10.addEventListener('click', () => {
+    deliver.src = 'brackets/bracket 6.png';
+})
+
+btn11.addEventListener('click', () => {
+    deliver.src = 'brackets/bracket 5.png';
+})
+
+btn12.addEventListener('click', () => {
+    deliver.src = 'brackets/bracket 4.png';
+})
+
+btn13.addEventListener('click', () => {
+    deliver.src = 'brackets/bracket 3.png';
+})
+
+btn14.addEventListener('click', () => {
+    deliver.src = 'brackets/bracket 2.png';
+})
+
+btn15.addEventListener('click', () => {
+    deliver.src = 'brackets/bracket 1.png';
+})
---
+++ script2.js
@@ -0,0 +1,56 @@
+let img = document.querySelector('#deliver');
+let btn1 = document.querySelector('#btn1');
+let btn2 = document.querySelector('#btn2');
+let btn3 = document.querySelector('#btn3');
+let btn4 = document.querySelector('#btn4');
+let btn5 = document.querySelector('#btn5');
+let btn6 = document.querySelector('#btn6');
+let btn7 = document.querySelector('#btn7');
+let btn8 = document.querySelector('#btn8');
+let btn9 = document.querySelector('#btn9');
+let btn10 = document.querySelector('#btn10');
+let btn11 = document.querySelector('#btn11');
+
+btn1.addEventListener('click', () => {
+    deliver.src = 'operations/op 1.png';
+})
+
+btn2.addEventListener('click', () => {
+    deliver.src = 'operations/op 2.png';
+})
+
+btn3.addEventListener('click', () => {
+    deliver.src = 'operations/op 3.png';
+})
+
+btn4.addEventListener('click', () => {
+    deliver.src = 'operations/op 4.png';
+})
+
+btn5.addEventListener('click', () => {
+    deliver.src = 'operations/op 5.png';
+})
+
+btn6.addEventListener('click', () => {
+    deliver.src = 'operations/op 6.png';
+})
+
+btn7.addEventListener('click', () => {
+    deliver.src = 'operations/op 7.png';
+})
+
+btn8.addEventListener('click', () => {
+    deliver.src = 'operations/op 8.png';
+})
+
+btn9.addEventListener('click', () => {
+    deliver.src = 'operations/op 9.png';
+})
+
+btn10.addEventListener('click', () => {
+    deliver.src = 'operations/op 10.png';
+})
+
+btn11.addEventListener('click', () => {
+    deliver.src = 'operations/op 11.png';
+})
